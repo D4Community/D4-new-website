e15edc4e77d08088250883caced3eaa56d98e362
ayush24k <ayushanshu69@gmail.com>
2024-08-18 18:25:24 +0530

    code format and fixes

--- app/(pages)/HeroPage.tsx
+++ app/(pages)/HeroPage.tsx
@@ -1,18 +1,28 @@
 import Image from "next/image";
-import { BlurLeft, BlurRight } from "./BlurElement";
+import { BlurLeft, BlurRight } from "../components/BlurElement";
 import HeroImage from "../assets/images/heroimg.png"
-import {OrangeButton} from "./Buttons";
+import {BlueButton, OrangeButton} from "../components/Buttons";
 
 import d4logo from "../assets/images/d4logo.png"
 
-export default function HeroSection() {
+export default function HeroPage() {
+    return (
+        <div>
+            <HeroSection />
+        </div>
+    )
+}
+
+// add hex images in hero section
+
+function HeroSection () {
     return (
         <div>
             <div className="text-white sm:py-36 py-20">
                 <div className="flex items-center justify-center gap-6 container z-10 relative">
-                    <div className="flex flex-col lg:text-left lg:items-start text-center items-center gap-10">
-                        <h1 className="sm:text-[55px] text-[50px] font-bold leading-[60px] bg-[linear-gradient(to_left,#A46FF2,#E49976)] text-transparent bg-clip-text [-webkit-background-clip:text]">Improve your programming skills significantly.</h1>
-                        <h3 className="sm:text-[24px] text-[18px]">With D4 Communities unique learning, practice, and coaching approach, you can master 70 programming languages. Exercising is enjoyable, beneficial, and completely free forever.</h3>
+                    <div className="flex flex-col lg:text-left lg:items-start text-center items-center md:gap-10 gap-8">
+                        <h1 className="sm:text-[55px] text-[48px] font-bold sm:leading-[60px] leading-[50px] bg-[linear-gradient(to_left,#A46FF2,#E49976)] text-transparent bg-clip-text [-webkit-background-clip:text]">Improve your programming skills significantly.</h1>
+                        <h3 className="sm:text-[24px] text-[16px]">With D4 Communities unique learning, practice, and coaching approach, you can master 70 programming languages. Exercising is enjoyable, beneficial, and completely free forever.</h3>
 
                         <div className="flex gap-5">
                             <OrangeButton label={"Join Now"} />
@@ -26,13 +36,13 @@ export default function HeroSection() {
                                     src={d4logo}
                                     alt={"d4 log0"} />
                             </div>
-                            <p className="max-w-[450px] font-light">is an autonomous, community financed, not-for-profit organisation.</p>
-                            <button className="border-[1px] p-2 border-[#67A9FD] rounded-lg text-[#67A9FD] text-[14px] px-4">Learn More</button>
+                            <p className="max-w-[450px] font-light sm:text-[16px] text-[14px]">is an autonomous, community financed, not-for-profit organisation.</p>
+                            <BlueButton label={"learn more"} />
                         </div>
                     </div>
                     <div className=" hidden lg:flex">
                         <Image
-                            className="w-[full]"
+                            className="w-[1020px]"
                             src={HeroImage}
                             alt="hero image"
                         />
@@ -43,5 +53,3 @@ export default function HeroSection() {
         </div>
     )
 }
-
-// add hex images
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import HeroSection from "./components/Hero";
+import HeroPage from "./(pages)/HeroPage";
 import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <div className="bg-[#0E0C15]">
       <Navbar />
-      <HeroSection />
+      <HeroPage />
     </div>
   );
 }

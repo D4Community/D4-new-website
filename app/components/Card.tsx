abae31c85d2b3863b1999e5a410ad283e866063c
ayush24k <ayushanshu69@gmail.com>
2024-09-07 17:29:43 +0530

    added some about sections

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -2,9 +2,12 @@ import Image from "next/image";
 
 
 export default function Card({ src, title, description }: any) {
+
+    
+
     return (
         <div className="p-4 flex flex-col gap-3">
-            <Image className="w-[72px]" src={src} alt="code icon" />
+            {src ? <Image className="w-[72px]" src={src} alt="code icon" /> : <span></span> }
             <div>
                 <h3 className="text-[24px] font-bold">{title}</h3>
                 <p className="text-[18px] font-light pt-2">{description}</p>
